cb85ecf7e0db5fc88bc1d768da865445f8b153b1
ayna.loginova.04216@mail.ru <ayna.loginova.04216@mail.ru>
2024-11-25 20:56:50 +0300

    ю.дл

--- SkyyTravel/Base/Avia.Designer.cs
+++ SkyyTravel/Base/Avia.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\student\source\repos\SkyyTravel\SkyyTravel\Base\Avia.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\aynal\Source\Repos\callutic\SkyyTravel\SkyyTravel\Base\Avia.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
