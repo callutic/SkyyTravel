18223ad150f4fcb4daab40417d4fd24db0cd30a4
DipperMika <swato@DESKTOP-LSF8225>
2024-11-27 22:13:46 +0300

    крч лаг баг ща

--- SkyyTravel/WinFolder/Avtoriz.xaml.cs
+++ SkyyTravel/WinFolder/Avtoriz.xaml.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
 
 namespace SkyyTravel.WinFolder
 {
@@ -27,6 +27,11 @@ namespace SkyyTravel.WinFolder
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
